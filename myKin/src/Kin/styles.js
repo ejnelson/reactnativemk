747f582ce9b5ee1bc443b74b29a7ab4462de5acf
Jacob Froman <jacob.h.froman@gmail.com>
2017-03-20 15:51:41 -0500

    added back containerPadded style

--- myKin/src/Kin/styles.js
+++ myKin/src/Kin/styles.js
@@ -4,54 +4,61 @@ import navStyles from '../Shared/NavBar/style';
 import { defaultBackgroundColor, borderColor } from '../style/colors';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: defaultBackgroundColor,
-        justifyContent: 'flex-start',
-        alignItems: 'stretch'
-    },
-    row: {
-        flex: 1,
-        justifyContent: 'space-between',
-        padding: 20,
-        borderBottomWidth: 1,
-        borderColor
-    },
-    header: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    name: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        paddingRight: 10
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: '#d7d7d7',
-        height: 50,
-        margin: 10,
-        padding: 10,
-        borderRadius: 5
-    },
-    detailInput: {
-        borderWidth: 1,
-        borderColor: '#d7d7d7',
-        height: 50,
-        margin: 10,
-        borderRadius: 5,
-        alignItems: 'center'
-    },
-    addIcon: {
-        margin: 10,
-        height: 30,
-        width: 30
-    }
+  container: {
+    flex: 1,
+    backgroundColor: defaultBackgroundColor,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
+  containerPadded: {
+    flex: 1,
+    backgroundColor: defaultBackgroundColor,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    padding: 10,
+  },
+  row: {
+    flex: 1,
+    justifyContent: 'space-between',
+    padding: 20,
+    borderBottomWidth: 1,
+    borderColor,
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    paddingRight: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#d7d7d7',
+    height: 50,
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
+  detailInput: {
+    borderWidth: 1,
+    borderColor: '#d7d7d7',
+    height: 50,
+    margin: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  addIcon: {
+    margin: 10,
+    height: 30,
+    width: 30,
+  },
 });
 
 export default {
-    ...globalStyles,
-    ...navStyles,
-    ...styles
+  ...globalStyles,
+  ...navStyles,
+  ...styles,
 };
